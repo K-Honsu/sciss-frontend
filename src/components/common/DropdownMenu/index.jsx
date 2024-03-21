acbb2a28c5acea9a39482bd1887b4217ff14d520
K-Honsu <feranmia51@gmail.com>
2024-03-21 19:10:01 +0100

    fix: user information and initials

--- src/components/common/DropdownMenu/index.jsx
+++ src/components/common/DropdownMenu/index.jsx
@@ -1,14 +1,16 @@
 import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
+import { usebackendStore } from "../../../store/store";
 
 function DropdownMenu() {
+  const userInformation = usebackendStore((state) => state.user)
   const [isShown, setIsShown] = useState(false)
   return (
     <div className="relative inline-block text-left">
       <div>
         <button type="button" onClick={() => setIsShown(!isShown)} className="inline-flex items-center w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true">
-          <span className='aspect-square w-10 bg-gray-700 rounded-full flex items-center justify-center text-xl text-neutral-200'>L</span>
-          LazyCoder
+          <span className='aspect-square w-10 bg-gray-700 rounded-full flex items-center justify-center text-xl text-neutral-200'>{userInformation.userName[0]}</span>
+          {userInformation.userName}
           <svg className="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
             <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
           </svg>
